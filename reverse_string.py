3afb58c3740026d742707f5f6d1a93bb0f733c32
Long Nguyen <longnguyenhai@vccorp.vn>
2011-11-02 10:02:05 +0700

    rever string

---
+++ reverse_string.py
@@ -0,0 +1,10 @@
+# string input
+string=raw_input ("Nhap vao chuoi: ")
+# convert string to list
+list = list(string)
+# reverse a list
+list.reverse()
+# join and print list as a string
+print  "Chuoi dao nguoc:",''.join(list)
+
+## xem lai ham ''.join(list)
