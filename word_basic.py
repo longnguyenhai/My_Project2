e9bff3d40b659d6d4dfac0b11bfba338ccfa216a
Long Nguyen <longnguyenhai@vccorp.vn>
2011-11-08 14:59:05 +0700

    basic word

--- word_basic.py
+++ word_basic.py
@@ -1,15 +1,22 @@
 # In put a file
-fo = open('z.py','r')
-#line = fo.readlines()'
+file_input = raw_input ("File input:")
+fo = open(file_input,'r')
 count = 0
+word_list = []
 for line in fo:
-	line  = (line.strip()).split()
-	#print line
-	count = count +1
+	line  = (line.strip()).split()          # strip white space and split word to a list
+	count = count +1                      # count line
 	for word in line:
-		freq=line.count(word)
-		print freq,word,count
-
-
-
-
+		word= word.lower()            # lower word
+		freq=line.count(word)        #  count frequence word 
+		for elem in word_list:
+			if elem[0] == word:
+				elem[1] += 1
+				if count not in elem[2]:
+					elem[2].append(count)
+				break
+		else:
+			word_list.append( [word,freq,[count]] )
+            
+for i in word_list:
+	print "%2d %s %d %d" %(i[1],i[0], i[2][0], i[2][1])
